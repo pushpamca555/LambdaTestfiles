386811733f57439b1b2476c23e4891365c210165
pushpamca555 <pushpamca555@gmail.com>
2024-12-17 12:04:06 +0530

    Update test_form_inputs.py

--- LambdaTest/Tests/test_form_inputs.py
+++ LambdaTest/Tests/test_form_inputs.py
@@ -15,12 +15,7 @@ class TestScenario3(CommonTests):
         # Click submit without entering input field values
         base_page.click_on_element(TestData.form_submit_button)
 
-        """
-        I noticed there has been an update in the UI due to which the error message for empty fields seems to be 
-        detached from the DOM.
-        Due to this, the error message element cannot be located using conventional locator strategies.
-        Hence, I have commented out the assertion part for the warning text below:
-        """
+        
         # input_forms_page.validate_warning_message_for_input_fields()
 
         # Enter form details
